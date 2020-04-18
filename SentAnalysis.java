42df9bcbf309357345a5de37d13ae4d7a227a503
Emma Neary <nearyemm@gmail.com>
2020-04-17 17:08:16 -0700

    counted word occurences in train

--- SentAnalysis.java
+++ SentAnalysis.java
@@ -1,22 +1,27 @@
 /*
- * Please see submission instructions for what to write here. 
+ * Please see submission instructions for what to write here.
  */
 
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SentAnalysis {
 
-	final static File TRAINFOLDER = new File("train-s20");
-	
-		
+	final static String TRAINFOLDERNAME = "train";
+
+	final static File TRAINFOLDER = new File("train");
+
+	private static Map<String, Integer> negativeCount = new HashMap<>();
+	private static Map<String, Integer> positiveCount = new HashMap<>();
+
 	public static void main(String[] args) throws IOException
-	{	
-		ArrayList<String> files = readFiles(TRAINFOLDER);		
-		
+	{
+		ArrayList<String> files = readFiles(TRAINFOLDER);
+
 		train(files);
 		//if command line argument is "evaluate", runs evaluation mode
 		if (args.length==1 && args[0].equals("evaluate")){
@@ -29,53 +34,82 @@ public class SentAnalysis {
 			String textToClassify = scan.nextLine();
 			System.out.println("Result: "+classify(textToClassify));
 		}
-		
-	}
-	
 
-	
+	}
+
+
+
 	/*
-	 * Takes as parameter the name of a folder and returns a list of filenames (Strings) 
+	 * Takes as parameter the name of a folder and returns a list of filenames (Strings)
 	 * in the folder.
 	 */
 	public static ArrayList<String> readFiles(File folder){
-		
+
 		System.out.println("Populating list of files");
-		
+
 		//List to store filenames in folder
 		ArrayList<String> filelist = new ArrayList<String>();
-		
-	
+
+
 		for (File fileEntry : folder.listFiles()) {
 	        String filename = fileEntry.getName();
 	        filelist.add(filename);
 		}
-	    
+
 		/*
 		for (String fileEntry : filelist) {
 	        System.out.println(fileEntry);
 		}
-		
+
 		System.out.println(filelist.size());
 		*/
-		
-		
+
+
 		return filelist;
 	}
-	
-	
 
-	
+
+
+
 	/*
 	 * TO DO
-	 * Trainer: Reads text from data files in folder datafolder and stores counts 
+	 * Trainer: Reads text from data files in folder datafolder and stores counts
 	 * to be used to compute probabilities for the Bayesian formula.
 	 * You may modify the method header (return type, parameters) as you see fit.
 	 */
 	public static void train(ArrayList<String> files) throws FileNotFoundException
 	{
-		
+			Map<String, Integer> m;
+			char rating;
+			Scanner scan;
+
+			for (String filename: files){
+					rating = filename.charAt(filename.indexOf('-') + 1);
+					if (rating=='1'){
+						m = negativeCount;
+					} else {
+						m = positiveCount;
+					}
+					scan = new Scanner(new File(TRAINFOLDERNAME + "/" + filename));
+					scan.useDelimiter(("[ )('\"/\\:;@,!?.-]+"));
+					String s;
+					while (scan.hasNext()){
+						s = scan.next();
+						s.toLowerCase();
+						m.put(s, m.getOrDefault(s, 0) + 1);
+					}
+			}
+
+			/* for (Map.Entry<String, Integer> entry : m.entrySet()) {
+					System.out.println(entry.getKey() + " = " + entry.getValue());
+			} */
+			//System.out.println(negativeCount.size());
+			//System.out.println(positiveCount.size());
+			scan.close();
 	}
+
+
+
 
 
 	/*
@@ -84,34 +118,34 @@ public class SentAnalysis {
 	public static String classify(String text)
 	{
 		String result="";
-		
+
 		return result;
-		
+
 	}
-	
-	
-	
+
+
+
 
 	/*
 	 * TO DO
 	 * Classifier: Classifies all of the files in the input folder (type: File) as positive or negative
 	 * You may modify the method header (return type, parameters) as you like.
 	 */
-	public static void evaluate() throws FileNotFoundException 
+	public static void evaluate() throws FileNotFoundException
 	{
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Enter folder name of files to classify: ");
 		String foldername = scan.nextLine();
 		File folder = new File(foldername);
-		
+
 		ArrayList<String> filesToClassify = readFiles(folder);
-		
-		
-		
+
+
+
 	}
-	
-	
-	
+
+
+
 }
