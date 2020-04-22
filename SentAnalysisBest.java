9befbf2ff4d5c01ab980e97bb50b6adafcf53ba5
pmehler <pmehler@middlebury.edu>
2020-04-21 21:51:58 -0400

    removed print statements

--- SentAnalysisBest.java
+++ SentAnalysisBest.java
@@ -18,8 +18,8 @@ public class SentAnalysisBest {
 	private static Map<String, Integer> negativeCount = new HashMap<>();
 	private static Map<String, Integer> positiveCount = new HashMap<>();
 
-	private static int[] pos_text_len = new int[1000];
-	private static int[] neg_text_len = new int[1000];
+	private static int[] pos_text_len = new int[500];
+	private static int[] neg_text_len = new int[500];
 
 	private static double num_positive_reviews = 0;
 	private static double num_negative_reviews = 0;
@@ -59,7 +59,6 @@ public class SentAnalysisBest {
 	}
 
 
-
 	/*
 	 * Takes as parameter the name of a folder and returns a list of filenames (Strings)
 	 * in the folder.
@@ -77,99 +76,70 @@ public class SentAnalysisBest {
 	        filelist.add(filename);
 		}
 
-		/*
-		for (String fileEntry : filelist) {
-	        System.out.println(fileEntry);
-		}
-
-		System.out.println(filelist.size());
-		*/
-
-
 		return filelist;
 	}
 
 
-
-
-	/*
-	 * TO DO
-	 * Trainer: Reads text from data files in folder datafolder and stores counts
-	 * to be used to compute probabilities for the Bayesian formula.
-	 * You may modify the method header (return type, parameters) as you see fit.
-	 */
 	public static void train(ArrayList<String> files) throws FileNotFoundException
 	{
-			Map<String, Integer> m;
-			char rating;
-			Scanner scan;
-			int[] exclamations;
-			int[] numCapsArr;
+		Map<String, Integer> m;
+		char rating;
+		Scanner scan;
+		int[] exclamations;
+		int[] numCapsArr;
 
-			for (String filename: files){
-					rating = filename.charAt(filename.indexOf('-') + 1);
-					if (rating=='1'){
-						num_negative_reviews++;
-						m = negativeCount;
-						exclamations = numExclamationMarksNeg;
-						numCapsArr = numCapsNeg;
-					}
-					else {
-						num_positive_reviews++;
-						m = positiveCount;
-						exclamations = numExclamationMarksPos;
-						numCapsArr = numCapsPos;
-					}
-					scan = new Scanner(new File(TRAINFOLDERNAME + "/" + filename));
-
-					while (scan.hasNext()){
-						String text = scan.nextLine();
-						String capsOnly = text.replaceAll("[^A-Z]+", "");
-						String exclamationsOnly = text.replaceAll("[^!]+", "");
-						int numExclamationsinReview = exclamationsOnly.length();
-						if (numExclamationsinReview<500){
-							exclamations[numExclamationsinReview]++;
-						}
-						if(capsOnly.length() < 500){
-							numCapsArr[capsOnly.length()]++;
-						}
-						int text_length = text.length();
-						//hash
-						text_length = (int) (text_length/10);
-
-
-						if (rating=='1'){
-							neg_text_len[text_length]++;
-						}
-						else {
-							pos_text_len[text_length]++;
-						}
-						text = text.toLowerCase();
-
-						String [] words = text.split("[ )('\"/\\:;@,!?.-]+");
-
-						for(String s: words){
-							m.put(s, m.getOrDefault(s, 0) + 1);
-						}
-					}
-				scan.close();
+		for (String filename: files){
+			rating = filename.charAt(filename.indexOf('-') + 1);
+			if (rating=='1'){
+				num_negative_reviews++;
+				m = negativeCount;
+				exclamations = numExclamationMarksNeg;
+				numCapsArr = numCapsNeg;
 			}
+			else {
+				num_positive_reviews++;
+				m = positiveCount;
+				exclamations = numExclamationMarksPos;
+				numCapsArr = numCapsPos;
+			}
+			scan = new Scanner(new File(TRAINFOLDERNAME + "/" + filename));
 
-			//System.out.println("num pos: " + num_positive_reviews);
-			//System.out.println("num neg: " + num_negative_reviews);
-
-/*
-			for(int i=0; i<(neg_text_len.length-700); i++){
-				System.out.println("bin number: " + i);
-				System.out.println("negative text length: " + neg_text_len[i]);
-				System.out.println("positive text length: " + pos_text_len[i]);
-				System.out.println();
+			while (scan.hasNext()){
+				String text = scan.nextLine();
+				String capsOnly = text.replaceAll("[^A-Z]+", "");
+				String exclamationsOnly = text.replaceAll("[^!]+", "");
+				int numExclamationsinReview = exclamationsOnly.length();
+				if (numExclamationsinReview<500){
+					exclamations[numExclamationsinReview]++;
 				}
-*/
+				if(capsOnly.length() < 500){
+					numCapsArr[capsOnly.length()]++;
+				}
+				int text_length = text.length();
+				//hash
+				text_length = (int) (text_length/10);
+
+				if (text_length>500) {
+					text_length = 499;
+				}
+
+				if (rating=='1'){
+					neg_text_len[text_length]++;
+				}
+				else {
+					pos_text_len[text_length]++;
+				}
+				text = text.toLowerCase();
+
+				String [] words = text.split("[ )('\"/\\:;@,!?.-]+");
+
+				for(String s: words){
+					m.put(s, m.getOrDefault(s, 0) + 1);
+				}
+			}
+			scan.close();
+		}
 	}
-
-
-
 
 
 	/*
@@ -182,11 +152,11 @@ public class SentAnalysisBest {
 		double smoothing_coef = 0.001;
 
 		int text_length_hash = (int) (text.length()/10);
+		if (text_length_hash>500) {
+			text_length_hash = 499;
+		}
 		double pos_len_prob = (pos_text_len[text_length_hash]+smoothing_coef)/num_positive_reviews;
 		double neg_len_prob = (neg_text_len[text_length_hash]+smoothing_coef)/num_negative_reviews;
-
-		//System.out.println("positive length hash: " + pos_text_len[text_length_hash]);
-		//System.out.println("negative length hash: " + neg_text_len[text_length_hash]);
 
 		double pos_sum = 0;
 		double neg_sum = 0;
@@ -196,80 +166,44 @@ public class SentAnalysisBest {
 		text = text.toLowerCase();
 		String [] words = text.split("[ )('\"/\\:;@,!?.-]+");
 
-
-		/*
-			for(int i=0; i<words.length; i++){
-			System.out.println(words[i]);
-		}*/
 		double total_num_unique_pos_words = positiveCount.size();
 		double total_num_unique_neg_words = negativeCount.size();
-
 
 		double prob_positive = num_positive_reviews / (num_positive_reviews+num_negative_reviews);
 		for(int i=0; i<words.length; i++){
 			double prob_word_pos = (positiveCount.getOrDefault(words[i],0)+smoothing_coef)/(total_num_unique_pos_words+(smoothing_coef*words.length));
 			pos_sum = pos_sum + (Math.log(prob_word_pos));
-
-/*
-			System.out.println("word: " + words[i]);
-			System.out.println("occurences count: " + positiveCount.getOrDefault(words[i],0));
-
-			System.out.println("prob word pos: " + prob_word_pos);
-			System.out.println("num unique pos words" + total_num_unique_pos_words);
-			System.out.println("length of words" + words.length);
-			System.out.println("log: " + (Math.log(prob_word_pos) / Math.log(2)));
-			System.out.println("product: " + pos_sum);
-
-			System.out.println();
-
-*/
 		}
+
 		int numExclamCount = 0;
 		if (numExclamationMarks < 500){
 			numExclamCount = numExclamationMarksPos[numExclamationMarks];
 		}
+
 		double prob_exclamations_pos = (numExclamCount + smoothing_coef)/(num_positive_reviews + (smoothing_coef * words.length));
+		
 		int numCapsCount = 0;
 		if (numCaps < 500){
 			numCapsCount = numCapsPos[numCaps];
 		}
 		double prob_caps_pos = (numCapsCount + smoothing_coef)/(num_positive_reviews + (smoothing_coef * words.length));
 
-
 		double prob_text_pos = pos_sum + Math.log(prob_positive);
 		prob_text_pos = prob_text_pos*0.503;
-/*
-		System.out.println("prob pos exclamations: " + prob_exclamations_pos);
-		System.out.println("prob pos caps: " + prob_caps_pos);
-*/
+
 		// New Feature
 		prob_text_pos += Math.log(pos_len_prob);
 		prob_text_pos += Math.log(prob_exclamations_pos);
 		prob_text_pos += Math.log(prob_caps_pos);
-		//System.out.println("positive log: " + Math.log(pos_len_prob));
 
 		double prob_negative = num_negative_reviews / (num_positive_reviews+num_negative_reviews);
 		for(int i=0; i<words.length; i++){
 			double prob_word_neg = (negativeCount.getOrDefault(words[i],0)+smoothing_coef)/(total_num_unique_neg_words+(smoothing_coef*words.length));
 			neg_sum = neg_sum + (Math.log(prob_word_neg));
-
-/*
-			System.out.println("word: " + words[i]);
-			System.out.println("occurences count: " + negativeCount.getOrDefault(words[i],0));
-			System.out.println("num unique neg words" + total_num_unique_neg_words);
-			System.out.println("prob word neg: " + prob_word_neg);
-			System.out.println("length of words" + words.length);
-
-
-			System.out.println("log: " + (Math.log(prob_word_neg) / Math.log(2)));
-			System.out.println("product: " + neg_sum);
-
-			System.out.println();
-*/
 		}
+
 		double prob_text_neg = neg_sum + Math.log(prob_negative);
 		prob_text_neg = prob_text_neg*0.497;
-		//System.out.println("negative log: " + Math.log(neg_len_prob));
 
 
 		numExclamCount = 0;
@@ -282,41 +216,22 @@ public class SentAnalysisBest {
 			numCapsCount = numCapsNeg[numCaps];
 		}
 		double prob_caps_neg = (numCapsCount + smoothing_coef)/(num_negative_reviews + (smoothing_coef * words.length));
-/*
-		System.out.println("prob neg exclamations: " + prob_exclamations_neg);
-		System.out.println("prob neg caps: " + prob_caps_neg);
-*/
 
 		// New Feature
 		prob_text_neg += Math.log(neg_len_prob);
 		prob_text_neg += Math.log(prob_exclamations_neg);
 		prob_text_neg += Math.log(prob_caps_neg);
 
-		/*
-		System.out.println("prob_negative: " + prob_negative);
-		System.out.println("prob_positive: " + prob_positive);
-		*/
 		if (prob_text_neg > prob_text_pos){
 			result = "negative";
 		}
 		else{
 			result = "positive";
 		}
-		/*
-		System.out.println("prob pos: " + prob_text_pos);
-		System.out.println("prob neg: " + prob_text_neg);
-		*/
 		return result;
 	}
 
 
-
-
-	/*
-	 * TO DO
-	 * Classifier: Classifies all of the files in the input folder (type: File) as positive or negative
-	 * You may modify the method header (return type, parameters) as you like.
-	 */
 	public static void evaluate() throws FileNotFoundException
 	{
 		@SuppressWarnings("resource")
@@ -362,21 +277,5 @@ public class SentAnalysisBest {
 			System.out.println("Accuracy: " + (totalCorrect/totalClassified) * 100);
 			System.out.println("Precision (Positive): " + ((double)numCorrectPositive/(double)totalClassifiedPositive)*100);
 			System.out.println("Precision (Negative): " + ((double)numCorrectNegative/(double)totalClassifiedNegative)*100);
-
-/*
-
-• Accuracy: number correctly classified / total classified
-
-• Precision: number correctly classified as positive / total classified as positive
-	       number correctly classified as negative / total classified as negative
-
-Total classified pos, neg: 15642 11540
-num correct pos, neg: 11718 9259
-Total correct, total classified: 20977.0 27182.0
-Accuracy: 77.17239349569567
-Precision (Positive): 74.91369390103567
-Precision (Negative): 80.23396880415945
-*/
-
 	}
 }
